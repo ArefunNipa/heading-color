6ba39376077a3151488180c19a74991eb18e6317
ArefunNipa <arefunnnipa@gmail.com>
2024-10-27 20:36:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+
+  return (
+    <>
+    <Heading className={"font-bold text-[64px] text-center text-amber-600"} text={"Find The #BestFashion# Style For You"}/>
+       
+    </>
+  )
+}
+
+export default App
